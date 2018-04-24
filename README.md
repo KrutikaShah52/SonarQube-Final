8063a7162e0462c472b7523f9b5e39eeccaabc1d
Krutika Shah <krutika.shah@einfochips.com>
2018-04-24 16:37:31 +0530

    Renew code push onto the new repository

---
+++ README.md
@@ -0,0 +1,13 @@
+# Code-Coverage
+
+Make one palindrom project which shows the string is palindrom is not.
+
+Written 2 test cases using Junit.
+
+Use Jacoco and cobertura.
+
+Useing both tech generate graphical presentation of the project.
+
+this Tech generate report for code-coverage using Junit.
+
+
---
+++ src/main/java/com/test/Jacoco_Maven_Example/PalindromeExample.java
@@ -0,0 +1,27 @@
+package com.test.Jacoco_Maven_Example;
+
+public class PalindromeExample {
+
+	public boolean isPalindrome(String inputString) {
+	    if (inputString.length() == 0) {
+	        return true;
+	    } else {
+	        char firstChar = inputString.charAt(0);
+	        char lastChar = inputString.charAt(inputString.length() - 1);
+	        String mid = inputString.substring(1, inputString.length() - 1);
+		    for(int i=0; i<=5; i++)
+		    {
+			    if(i==4)
+				    break;
+			    System.out.println(" " +i);
+		    }
+	        return (firstChar == lastChar) && isPalindrome(mid);
+	    }
+	}
+	public void add()
+	{
+		System.out.println("Adding something");
+		System.out.println("Adding something new");
+	}
+	
+}
